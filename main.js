cb2d1eff538560a6867145b49a9d1970f0fff0a6
Avinash Prasad <avinaspr@amdocs.com>
2021-07-05 20:28:09 +0530

    fixed bugs

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-Add.java(x+y);
-Difference.java(x+y);
+class Addition {
+  public int java(int sum) {
+    return sum;
+  }
+}
+
+class Subtraction{
+  public int java(int diff) {
+    return diff;
+  }
+}
+
+class Main {
+  public static void main(String args[]) {
+    
+    Addition Add = new Addition();
+    Add.java(x+y);
+
+    Subtraction Difference = new Subtraction();
+    Difference.java(x-y);
+  }
+}
